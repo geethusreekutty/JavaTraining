8211b4f34967c73042ebb4cfd0045c2bd921003a
geethusreekutty <geethusreekutty94@gmail.com>
2023-09-06 22:30:11 +0530

    First Commit

---
+++ Account.java
@@ -0,0 +1,14 @@
+public class Account
+{
+    public static void main(String[] args) 
+    {
+        
+        System.out.println("**********Account Details***********");
+        System.out.println("Account Holder Name:Geethu Thankaraj");
+        System.out.println("Account Type:Savings");
+        System.out.println("Branch Name:ICICI");
+        System.out.println("IFSC Code:ICICI000");
+        System.out.println("Account Number:123456789");
+
+    }
+}
