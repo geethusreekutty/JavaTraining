ed4172905a629a0589d83301ed0e2668903b98f5
geethusreekutty <geethusreekutty94@gmail.com>
2023-09-07 22:55:36 +0530

    datatypessample

--- Account.java
+++ Account.java
@@ -2,13 +2,17 @@ public class Account
 {
     public static void main(String[] args) 
     {
-        
-        System.out.println("**********Account Details***********");
-        System.out.println("Account Holder Name:Geethu Thankaraj");
+        String a="Account Holder Name:Geethu Thankaraj";
+        System.out.println(a);
+        String b="Account Type:Savings";
+        System.out.println(b);
+        String c="Branch Name:ICICI";
+        System.out.println(c);
+        /*System.out.println("Account Holder Name:Geethu Thankaraj");
         System.out.println("Account Type:Savings");
         System.out.println("Branch Name:ICICI");
         System.out.println("IFSC Code:ICICI000");
-        System.out.println("Address: Amigoz,Arasummood,TVM");
+        System.out.println("Address: Amigoz,Arasummood,TVM");*/
 
     }
 }
---
+++ DataTypes.java
@@ -0,0 +1,36 @@
+public class DataTypes {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println(sum);
+
+        char c1='b';
+        System.out.println(c1);
+
+        String a1="Account Holder Name:Geethu Thankaraj";
+        System.out.println(a1);
+
+        float i=1.2f;
+        float j=2.5f;
+        float k=i+j;
+        System.out.println(k);
+
+        double d=12.223d;
+        double e=24.444d;
+        double f=d+e;
+        System.out.println(d+e);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+
+
+
+
+    }
+    
+}
